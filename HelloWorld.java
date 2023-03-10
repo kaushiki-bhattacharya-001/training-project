259b13bbfe3ed27b007d0aac4522c60e11e44642
kaushiki.bhattacharya@sap.com <kaushiki.bhattacharya@sap.com>
2023-03-10 21:00:55 +0530

    Third Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String args[]) { 
 
 		System.out.println("HELLO WORLD!");
-		System.out.println("20 + 30 = "+(20+30));
+		int a = 10, b = 20;
+		System.out.println("a + b = "+(a+b));
 
 	}
 
