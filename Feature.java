db77d15c0e726ec6140d436e92115eca3eef2ba6
kaushiki.bhattacharya@sap.com <kaushiki.bhattacharya@sap.com>
2023-03-10 22:25:29 +0530

    Second Commit to main

--- Feature.java
+++ Feature.java
@@ -5,6 +5,8 @@ public class Feature {
 	public static void main(String args[]) {
 	
 		System.out.println("My Second Git Repo!");
+		String str1 = "Hello", str2 = "World";
+		System.out.println("String Concatenation: "+(str1 + " " + str2));
 
 	}
 
